bd9ce3689fd6db2817f43e826c5fde0719f10eb0
Steve Parker <stevenparker@dyson.com>
2013-07-24 11:23:27 +0100

    Up-issue for release 0.1.4.

--- app/src/misc/Version.h
+++ app/src/misc/Version.h
@@ -1,7 +1,7 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define GTS_BUILD_DATE "22/07/2013"
-#define GTS_BUILD_REVN "0.1.4i"
+#define GTS_BUILD_DATE "24/07/2013"
+#define GTS_BUILD_REVN "0.1.4"
 
 #endif // VERSION_H
