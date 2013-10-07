6babbe67fad70f20293d6f180bdabb4fed3c40f2
Akram Hussein <akram.hussein@dyson.com>
2013-10-07 10:26:03 +0100

    Bumped to v0.4.4

--- app/src/misc/Version.h
+++ app/src/misc/Version.h
@@ -1,7 +1,7 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define GTS_BUILD_DATE "04/10/2013"
-#define GTS_BUILD_REVN "0.4.3"
+#define GTS_BUILD_DATE "07/10/2013"
+#define GTS_BUILD_REVN "0.4.4"
 
 #endif // VERSION_H
